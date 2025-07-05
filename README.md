87f94438d98300ba370ab26fd08b0cfd75e63654
phkandresc <kacoraizaca@gmail.com>
2025-07-05 16:49:37 -0500

    Update project paths and improve README documentation for gestor_tareas_scala

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Un enum es una enumeración que define un conjunto de constantes. En este caso, 
 `Prioridad`: Define las prioridades posibles de una tarea (BAJA, MEDIA, ALTA).
 
 - ### Case Class
+Caracteristicas de una case class:
+
+- Los campos son inmutables.
+- Se genera automaticamente metodos toString, equals, hashCode, copy
+- Pattern matching: se puede usar en expresiones match
+- En este caso Tarea y Etiqueta implementan Serializable
+- Serializable es una interfaz de java que permite que un objeto pueda ser convertido en una secuencia de bytes, para ser almacenado y posteriormente reconstruido.
+- Se pueden comparar por valor.
--- src/main/scala/model/GestorTareas.scala
+++ src/main/scala/model/GestorTareas.scala
@@ -1,49 +1,5 @@
 package model
 
-import java.io.{FileInputStream, ObjectInputStream}
-import scala.util.{Try, Success, Failure}
-
 object GestorTareas {
-  private var tareas: List[Tarea] = List()
-
-  def cargarTareas(ruta: String): List[Tarea] = {
-    val resultado = Try {
-      val archivo = new FileInputStream(ruta)
-      val in = new ObjectInputStream(archivo)
-      val tareas = in.readObject().asInstanceOf[List[Tarea]]
-      in.close()
-      tareas
-    }
-
-    resultado match {
-      case Success(tareas) =>
-        println(s"✅ Se cargaron ${tareas.length} tareas desde '$ruta'")
-        tareas
-
-      case Failure(exception) =>
-        println(s"❌ Error al cargar tareas: ${exception.getMessage}")
-        List.empty[Tarea] // Devuelve una lista vacía si ocurre un error
-    }
-  }
-
-  def agregarTarea(t: Tarea): Unit = {
-
-  }
-
-  def eliminarTarea(id: Int): Unit = {
-
-  }
-
-  def buscarPorEtiqueta(etiqueta: String): List[Tarea] = {
-    println("buscar por etiqueta")
-    List.empty[Tarea]
-  }
-
-  def filtrarPorEstado(e: Estado): List[Tarea] = {
-    println("filtrar por estado")
-    List.empty[Tarea]
-  }
-
-
 
 }
